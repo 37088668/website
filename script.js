cc61cb920352197c8bfaf963b088bde7973d3faa
37088668 <114056376+37088668@users.noreply.github.com>
2024-07-16 22:08:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let origin = document.getElementById("first_src")
+origin.addEventListener("mouseover",function(){
+  this.src="https://pic.baike.soso.com/ugc/baikepic2/0/20191022145839-1416048562_jpeg_1280_833_293239.jpg/1284";
+})
+origin.addEventListener("mouseout",function(){
+  this.src="https://images.chinatimes.com/newsphoto/2022-11-08/656/20221108003069.jpg";
+})
